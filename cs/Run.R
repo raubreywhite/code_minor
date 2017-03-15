72f6c7442402700c0698a4cd92af9176fcc71978
Richard White <r.aubrey.white@gmail.com>
2017-03-15 10:36:20 +0200

    Cs

--- cs/Run.R
+++ cs/Run.R
@@ -11,76 +11,15 @@ suppressWarnings(suppressMessages(library(ggplot2)))
 
 assign("RUN_ALL", TRUE, envir=globalenv())
 
-GetScores <- function(href){
-  system(sprintf("curl -sS '%s' > /analyses/code_minor/cs/test.txt",href))
-  a <- XML::htmlParse("/analyses/code_minor/cs/test.txt")
-  tableNodes = XML::getNodeSet(a, "//div")
-  classes <- unlist(sapply(tableNodes,function(x){
-    retval <- XML::xmlGetAttr(x,"class")[[1]]
-    if(is.null(retval)) retval <- "x"
-    return(retval)
-  }))
-  matches <- tableNodes[[which(classes=="hotmatchbox")[1]]]
-  vals <- XML::xmlValue(matches)
-  vals <- stringr::str_extract_all(vals,"[0-9]*:[0-9]*\n")[[1]]
-  vals <- vals[vals!=":\n"]
-  vals <- gsub("\n","",vals)
-  vals <- paste0(vals,collapse=",")
-  return(vals)
-}
-
-GetMatches <- function(date="2016-11-01"){
-  fakeDate <- paste0(format.Date(as.Date(date),"%Y"),"-",
-                     gsub("^0","",format.Date(as.Date(date),"%m")),"-",
-                     format.Date(as.Date(date),"%d"))
-  system(sprintf("curl -sS 'http://www.hltv.org/?pageid=324&filter=1&clean=1' --data 'vod=false&intersect=false&highlight=false&stats=false&demo=false&offset=0&daterange=%s+to+%s' > /analyses/code_minor/cs/test.txt",fakeDate,fakeDate))
-  
-  data <- XML::readHTMLTable("/analyses/code_minor/cs/test.txt")[[1]]
-  data <- data[,1:2]
-  for(i in 1:2){
-    data[,i] <- as.character(data[,i])
-  }
-  data[,1] <- date
-  data$teamA <- ""
-  data$teamB <- ""
-  data$scoreA <- as.numeric(gsub("\\(","",stringr::str_extract(data$Teams,"\\([0-9]*")))
-  data$scoreB <- as.numeric(gsub("\\)","",stringr::str_extract(data$Teams,"[0-9]*\\)")))
-  data$temp <- stringr::str_extract(data$Teams,"[0-9]*-[0-9]*")
-  data$Teams <- apply(data,1,function(x){gsub(paste0(" \\(",x[7],"\\)"),"",x[2])})
-  data$Teams <- gsub("  "," ",data$Teams)
-  data$temp <- NULL
-  x <- stringr::str_split(data$Teams," vs ")
-  for(i in 1:nrow(data)){
-    data$teamA[i] <- x[[i]][1]
-    data$teamB[i] <- x[[i]][2]
-  }
-  data$Teams <- NULL
-  
-  a <- XML::htmlParse("/analyses/code_minor/cs/test.txt")
-  tableNodes = XML::getNodeSet(a, "//table//td//a")
-  
-  data$scores <- ""
-  data$href <- ""
-  for(i in 1:nrow(data)){
-    data$href[i] <- paste0("http://www.hltv.org/",XML::xmlGetAttr(tableNodes[[i*2]], "href"))
-    if(data$scoreA[i] + data$scoreB[i]<5){
-      data$scores[i] <- GetScores(data$href[i])
-    }
-  }
-  data$gameNum <- nrow(data):1
-  return(data)
-}
-
-today <- lubridate::today()
-days <- as.character(as.Date(as.Date("2013-01-1"):(today-1),origin="1970-01-01"))
-for(day in days){
-  if(file.exists(file.path(RPROJ$PROJRAW,paste0(day,'.RDS')))) next
-  print(day)
-  try({
-    d <- GetMatches(date=day)
-    print(d)
-    saveRDS(d,file=file.path(RPROJ$PROJRAW,paste0(day,'.RDS')))
-    print("Success")
-  },TRUE)
-}
-
+f <- list.files(file.path(RPROJ$PROJRAW))[1:5]
+d <- vector("list",length(f))
+for(i in 1:length(f)) d[[i]] <- readRDS(file.path(RPROJ$PROJRAW,f[i]))
+d <- rbindlist(d)
+d[,href:=NULL]
+d[,games:=stringr::str_count(scores,",")+1]
+numGames <- max(d$games)
+d[,paste0("S",1:numGames) := tstrsplit(scores, ",", fixed=TRUE)]
+d[is.na(S1),S1:=paste0(scoreA,":",scoreB)]
+d[,scoreA:=NULL]
+d[,scoreB:=NULL]
+d[,scores:=NULL]
